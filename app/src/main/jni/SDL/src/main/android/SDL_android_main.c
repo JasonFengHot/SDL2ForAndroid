867bfcc04cde269650ff815cca1c456fded3f3a2
fenghuibin <fenghuibin@ismartv.cn>
2017-04-18 22:22:02 +0800

    add bmp test

--- app/src/main/jni/SDL/src/main/android/SDL_android_main.c
+++ app/src/main/jni/SDL/src/main/android/SDL_android_main.c
@@ -12,6 +12,15 @@
                  Functions called by JNI
 *******************************************************************************/
 #include <jni.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "SDL.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* Called before SDL_main() to initialize JNI bindings in SDL library */
 extern void SDL_Android_Init(JNIEnv* env, jclass cls);
@@ -19,9 +28,98 @@ extern void SDL_Android_Init(JNIEnv* env, jclass cls);
 /* This prototype is needed to prevent a warning about the missing prototype for global function below */
 JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject array);
 
+typedef struct Sprite
+{
+	SDL_Texture* texture;
+	Uint16 w;
+	Uint16 h;
+} Sprite;
+
+/* Adapted from SDL's testspriteminimal.c */
+Sprite LoadSprite(const char* file, SDL_Renderer* renderer)
+{
+	Sprite result;
+	result.texture = NULL;
+	result.w = 0;
+	result.h = 0;
+
+    SDL_Surface* temp;
+
+    /* Load the sprite image */
+    temp = SDL_LoadBMP(file);
+    if (temp == NULL)
+	{
+        fprintf(stderr, "Couldn't load %s: %s\n", file, SDL_GetError());
+        return result;
+    }
+    result.w = temp->w;
+    result.h = temp->h;
+
+    /* Create texture from the image */
+    result.texture = SDL_CreateTextureFromSurface(renderer, temp);
+    if (!result.texture) {
+        fprintf(stderr, "Couldn't create texture: %s\n", SDL_GetError());
+        SDL_FreeSurface(temp);
+        return result;
+    }
+    SDL_FreeSurface(temp);
+
+    return result;
+}
+
+void draw(SDL_Window* window, SDL_Renderer* renderer, const Sprite sprite)
+{
+	int w, h;
+	SDL_GetWindowSize(window, &w, &h);
+	SDL_Rect destRect = {w/2 - sprite.w/2, h/2 - sprite.h/2, sprite.w, sprite.h};
+	/* Blit the sprite onto the screen */
+	SDL_RenderCopy(renderer, sprite.texture, NULL, &destRect);
+}
+
 
 int SDL_main(int argc, char *argv[])
 {
+
+SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    if(SDL_CreateWindowAndRenderer(0, 0, 0, &window, &renderer) < 0)
+        exit(2);
+
+	Sprite sprite = LoadSprite("image.bmp", renderer);
+    if(sprite.texture == NULL)
+        exit(2);
+
+    /* Main render loop */
+    Uint8 done = 0;
+    SDL_Event event;
+    while(!done)
+	{
+        /* Check for events */
+        while(SDL_PollEvent(&event))
+		{
+            if(event.type == SDL_QUIT || event.type == SDL_KEYDOWN || event.type == SDL_FINGERDOWN)
+			{
+                done = 1;
+            }
+        }
+
+
+		/* Draw a gray background */
+		SDL_SetRenderDrawColor(renderer, 0xA0, 0xA0, 0xA0, 0xFF);
+		SDL_RenderClear(renderer);
+
+		draw(window, renderer, sprite);
+
+		/* Update the screen! */
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(1000000);
+    }
+
+    exit(0);
+
+
     // A dummy main that is necessary to compile SDL2 statically.
     return 0;
 }
@@ -85,6 +183,9 @@ JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jc
     return status;
 }
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* __ANDROID__ */
 
 /* vi: set ts=4 sw=4 expandtab: */

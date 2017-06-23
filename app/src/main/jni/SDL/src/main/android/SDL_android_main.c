fd32b6bad6c7f2df16511dfd5ad93c2e034eba2a
fenghuibin <fenghuibin@ismartv.cn>
2017-06-23 17:05:05 +0800

    第2课 - 优化表面的加载和Blit

--- app/src/main/jni/SDL/src/main/android/SDL_android_main.c
+++ app/src/main/jni/SDL/src/main/android/SDL_android_main.c
@@ -12,126 +12,31 @@
                  Functions called by JNI
 *******************************************************************************/
 #include <jni.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-
-#include "SDL.h"
+#include "YourSourceHere.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* Called before SDL_main() to initialize JNI bindings in SDL library */
-extern void SDL_Android_Init(JNIEnv* env, jclass cls);
+extern void SDL_Android_Init(JNIEnv *env, jclass cls);
 
 /* This prototype is needed to prevent a warning about the missing prototype for global function below */
-JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject array);
+JNIEXPORT int JNICALL
+Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv *env, jclass cls, jobject array);
 
-typedef struct Sprite
-{
-	SDL_Texture* texture;
-	Uint16 w;
-	Uint16 h;
-} Sprite;
-
-/* Adapted from SDL's testspriteminimal.c */
-Sprite LoadSprite(const char* file, SDL_Renderer* renderer)
-{
-	Sprite result;
-	result.texture = NULL;
-	result.w = 0;
-	result.h = 0;
-
-    SDL_Surface* temp;
-
-    /* Load the sprite image */
-    temp = SDL_LoadBMP(file);
-    if (temp == NULL)
-	{
-        fprintf(stderr, "Couldn't load %s: %s\n", file, SDL_GetError());
-        return result;
-    }
-    result.w = temp->w;
-    result.h = temp->h;
-
-    /* Create texture from the image */
-    result.texture = SDL_CreateTextureFromSurface(renderer, temp);
-    if (!result.texture) {
-        fprintf(stderr, "Couldn't create texture: %s\n", SDL_GetError());
-        SDL_FreeSurface(temp);
-        return result;
-    }
-    SDL_FreeSurface(temp);
-
-    return result;
-}
-
-void draw(SDL_Window* window, SDL_Renderer* renderer, const Sprite sprite)
-{
-	int w, h;
-	SDL_GetWindowSize(window, &w, &h);
-	SDL_Rect destRect = {w/2 - sprite.w/2, h/2 - sprite.h/2, sprite.w, sprite.h};
-	/* Blit the sprite onto the screen */
-	SDL_RenderCopy(renderer, sprite.texture, NULL, &destRect);
-}
-
-
-int SDL_main(int argc, char *argv[])
-{
-
-SDL_Window *window;
-    SDL_Renderer *renderer;
-
-    if(SDL_CreateWindowAndRenderer(0, 0, 0, &window, &renderer) < 0)
-        exit(2);
-
-	Sprite sprite = LoadSprite("/sdcard/image.bmp", renderer);
-    if(sprite.texture == NULL)
-        exit(2);
-
-    /* Main render loop */
-    Uint8 done = 0;
-    SDL_Event event;
-    while(!done)
-	{
-        /* Check for events */
-        while(SDL_PollEvent(&event))
-		{
-            if(event.type == SDL_QUIT || event.type == SDL_KEYDOWN || event.type == SDL_FINGERDOWN)
-			{
-                done = 1;
-            }
-        }
-
-
-		/* Draw a gray background */
-		SDL_SetRenderDrawColor(renderer, 0xA0, 0xA0, 0xA0, 0xFF);
-		SDL_RenderClear(renderer);
-
-		draw(window, renderer, sprite);
-
-		/* Update the screen! */
-		SDL_RenderPresent(renderer);
-
-		SDL_Delay(1000000);
-    }
-
-    exit(0);
-
-
-    // A dummy main that is necessary to compile SDL2 statically.
-    return 0;
+int SDL_main(int argc, char *argv[]) {
+    return app_main(argc, argv);
 }
 
 /* Start up the SDL app */
-JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject array)
-{
+JNIEXPORT int JNICALL
+Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv *env, jclass cls, jobject array) {
     int i;
     int argc;
     int status;
     int len;
-    char** argv;
+    char **argv;
 
     /* This interface could expand with ABI negotiation, callbacks, etc. */
     SDL_Android_Init(env, cls);
@@ -148,8 +53,8 @@ JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jc
      */
     argv[argc++] = SDL_strdup("app_process");
     for (i = 0; i < len; ++i) {
-        const char* utf;
-        char* arg = NULL;
+        const char *utf;
+        char *arg = NULL;
         jstring string = (*env)->GetObjectArrayElement(env, array, i);
         if (string) {
             utf = (*env)->GetStringUTFChars(env, string, 0);
@@ -168,11 +73,9 @@ JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jc
 
 
     /* Run the application. */
-
     status = SDL_main(argc, argv);
 
     /* Release the arguments. */
-
     for (i = 0; i < argc; ++i) {
         SDL_free(argv[i]);
     }
---
+++ app/src/main/jni/src/YourSourceHere.h
@@ -0,0 +1,10 @@
+//
+// Created by huibin on 6/23/17.
+//
+
+#ifndef SDL2FORANDROID_YOURSOURCEHERE_H
+#define SDL2FORANDROID_YOURSOURCEHERE_H
+
+int app_main(int argc, char* argv[]);
+
+#endif //SDL2FORANDROID_YOURSOURCEHERE_H
